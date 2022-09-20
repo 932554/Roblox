112761b195c2294237ff4724d26d504f60afa2c2
932554 <61924694+932554@users.noreply.github.com>
2022-09-20 08:22:39 -0400

    Update Loader.lua

--- Libraries/Character Patcher/Loader.lua
+++ Libraries/Character Patcher/Loader.lua
@@ -4,7 +4,7 @@
 
 do
     local found, patcher = pcall(game.HttpGet, game, string.format("%s/%s.lua", "https://github.com/932554/Roblox/tree/main/Libraries/Character%20Patcher/Patcher/Patches", game.PlaceId))
-    if not found then -- if a game specific patch for your current game isn't found then load the default patcher
+    if not found or not patcher then
         patcher = game:HttpGet("https://raw.githubusercontent.com/932554/Roblox/main/Libraries/Character%20Patcher/Patcher/Module.lua")
     end
     return loadstring(patcher)()
